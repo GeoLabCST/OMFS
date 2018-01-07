59da6240e8fc0f4b5506abdebbb0c97752fcc9f9
aegistz <teerayoot5056@gmail.com>
2018-01-07 15:19:08 +0700

    sssss

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { TabsPage } from './../tabs/tabs';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, LoadingController, AlertController, Events} from 'ionic-angular';
+import {FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+
 import { RegisterPage } from '../register/register';
-/**
- * Generated class for the WelcomePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+
+//import { MapPage } from '../map/map';
 
 @IonicPage()
 @Component({
@@ -16,18 +15,80 @@ import { RegisterPage } from '../register/register';
 })
 export class WelcomePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  public reportForm : FormGroup;
+  public email_user : FormControl;
+  public pass_user : FormControl;
+  public res: any;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad WelcomePage');
+  constructor(
+    public fb : FormBuilder,  
+    private navCtrl : NavController, 
+    private loadingCtrl : LoadingController,
+    private alertCtrl : AlertController,
+    public http: HttpClient,
+    public events: Events
+  ){
+    this.email_user = fb.control('', Validators.required);
+    this.pass_user = fb.control('', Validators.required);
+    this.reportForm = fb.group({
+      'email_user': this.email_user, 
+      'pass_user': this.pass_user
+    })
   }
-
 
   signup(){
     this.navCtrl.push(RegisterPage);
   }
+
+  submit() {
+    let loader = this.loadingCtrl.create({content: "กำลังเข้าสู่ระบบ.."});  
+    let email_user = this.reportForm.controls['email_user'].value;
+    let pass_user = this.reportForm.controls['pass_user'].value;
+   
+    let data = JSON.stringify({
+      'email_user':email_user,
+      'pass_user':pass_user
+    });
+
+    loader.present();    
+    this.http.post('http://localhost/libs/checklogin_omfs.php', data)
+    .subscribe(res => {
+       this.res = res;
+       console.log(res);
+      
+      if (this.res.message == 'error') {
+         loader.dismiss();      
+          let alert=this.alertCtrl.create({
+            title: 'E-Mail หรือรหัสผ่านของท่านไม่ถูกต้อง!',
+            subTitle: 'กรุณาลองอีกครั้ง หรือสมัครสมาชิกใหม่',
+            buttons:['ok']
+          });
+          alert.present();     
+      }else if(this.res.message == 'success'){
+         loader.dismiss(); 
+          this.gotoindex();      
+          let alert=this.alertCtrl.create({
+            title: 'E-Mail และรหัสผ่านถูกต้อง',
+            subTitle: 'กำลังเข้าสู่ระบบ',
+            buttons:['ok']
+          });
+          // alert.present();     
+      }      
+    }, error => {
+      console.log("Oooops!");
+      loader.dismiss();
+    });
+  }  
+
   gotoindex(){
+  
+    // Sharing data using service
+    // this.shareService.setUserData(this.res);
     this.navCtrl.push(TabsPage);
   }
+
+  
+
+
+
 }
